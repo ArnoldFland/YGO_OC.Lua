868d12729fd71b9b0c98626311a37d0df4263e61
Arnold Fland <923777819@qq.com>
2025-03-19 15:16:09 +0800

    Add files via upload

---
+++ 千葉游慎/星界拂晓 寻星之魔术师/script/c20241111.lua
@@ -0,0 +1,140 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddCodeList(c,20241111)
+	-- Link summon
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsType,TYPE_PENDULUM),1,1,s.lcheck)
+	c:EnableReviveLimit()
+	
+	-- Effect 1: Discard a card to place a Pendulum monster from Deck into Pendulum Zone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(20241111,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,20241111)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	e1:SetCost(s.spcost)
+	c:RegisterEffect(e1)
+	-- Effect 2: Extra Pendulum Summon from Graveyard
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(20241111,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,20241112)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.ptg)
+	e2:SetOperation(s.pop)
+	c:RegisterEffect(e2)
+end
+
+-- Link summon 
+function s.lcheck(g,lc,tp)
+	return g:IsExists(Card.IsLinkSetCard,1,nil,0x985,0x98)
+end
+
+function s.setfilter(c)
+	return c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+end
+
+-- Effect 1 cost 
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+
+-- place P monster from Deck to P Zone
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1))
+			and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil)
+	end
+end
+
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if not (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+-- EXP
+function s.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local c=e:GetHandler()
+		local lpz=Duel.GetFieldCard(tp,LOCATION_PZONE,0)
+		local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+		if lpz==nil or rpz==nil then return false end
+		local loc=0
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_HAND end
+		if Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM)>0 then loc=loc+LOCATION_EXTRA end
+		if loc==0 then return false end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_EXTRA_PENDULUM_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1, 0)
+		e1:SetValue(aux.TRUE)
+		e1:SetReset(RESET_PHASE + PHASE_END)
+		Duel.RegisterEffect(e1, tp)
+		local eset={e1}
+		local lscale=lpz:GetLeftScale()
+		local rscale=rpz:GetRightScale()
+		if lscale>rscale then lscale,rscale=rscale,lscale end
+		local g=Duel.GetFieldGroup(tp,loc,0)
+		local res=g:IsExists(aux.PConditionFilter,1,nil,e,tp,lscale,rscale,eset)
+		e1:Reset()
+		return res
+	end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end
+--
+function s.pop(e,tp,eg,ep,ev,re,r,rp)
+	local c = e:GetHandler()
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_PENDULUM_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1, 0)
+	e1:SetValue(aux.TRUE)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local eset={e1}
+	local lpz=Duel.GetFieldCard(tp,LOCATION_PZONE,0)
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	local lscale=lpz:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale, rscale=rscale,lscale end
+	local loc=0
+	local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft2=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM)
+	local ft=Duel.GetUsableMZoneCount(tp)
+	local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+	if ect and ect<ft2 then ft2=ect end
+	if Duel.IsPlayerAffectedByEffect(tp, 59822133) then
+		if ft1>0 then ft1=1 end
+		if ft2>0 then ft2 =1 end
+		ft=1
+	end
+	if ft1>0 then loc=loc | LOCATION_HAND end
+	if ft2>0 then loc=loc | LOCATION_EXTRA end
+	local tg=Duel.GetMatchingGroup(aux.PConditionFilter,tp,loc,0,nil,e,tp,lscale,rscale,eset)
+	tg=tg:Filter(aux.PConditionExtraFilterSpecific,nil,e,tp,lscale,rscale,e1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	aux.GCheckAdditional=aux.PendOperationCheck(ft1,ft2,ft)
+	local g=tg:SelectSubGroup(tp,aux.TRUE,false,1, math.min(#tg,ft))
+	aux.GCheckAdditional=nil
+	if not g then
+		e1:Reset()
+		return
+	end
+	local sg=Group.CreateGroup()
+	sg:Merge(g)
+	Duel.HintSelection(Group.FromCards(lpz))
+	Duel.HintSelection(Group.FromCards(rpz))
+	Duel.RaiseEvent(sg,EVENT_SPSUMMON_SUCCESS_G_P,e,REASON_EFFECT,tp,tp,0)
+	Duel.SpecialSummon(sg,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
+	e1:Reset()
+end
